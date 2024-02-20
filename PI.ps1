c347455e249c9297695fe9dcfe0ccb20b892c983
tejagitacc <67002975+tejagitacc@users.noreply.github.com>
2024-02-20 10:25:08 +0530

    Update PI.ps1

--- PI.ps1
+++ PI.ps1
@@ -1,2 +1,4 @@
 $PI = 3.14
-Write-Host "The value of PI is $PI"
+Write-Host "The value of PI is $PI"      //Prints The value of PI is 3.14
+Write-Host "The value of `$PI is $PI"    //Prints The value of $PI is 3.14
+Write-Host "Addition of 1 with `$PI is $($PI+1)" //Prints Addition of 1 with $PI is 4.14
